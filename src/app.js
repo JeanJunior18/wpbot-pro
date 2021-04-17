eaf509876feaa7e23ede51c11bda93d3531719bf
Jean Junior <jclgjunior@gmail.com>
2021-04-16 22:37:54 -0300

    feat: Implement Router

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 
+import router from './router';
+
 class App {
   constructor() {
     this.app = express();
     this.server = http.Server(this.app);
+
+    this.middlewares();
+    this.router();
 
     console.log('Instanced Server');
   }
 
   middlewares() {
     this.app.use(cors());
+  }
+
+  router() {
+    this.app.use(router);
   }
 }
 
