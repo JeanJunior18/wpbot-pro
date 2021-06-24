6d05d15a072a4e6a880424ebf5b4eb848faea4b2
Jean Junior <jclgjunior@gmail.com>
2021-06-24 11:29:24 -0300

    Client data on firebase management

--- src/app/libs/Venom.js
+++ src/app/libs/Venom.js
@@ -4,22 +4,25 @@ const firebase = require('../../firebase');
 
 class VenomClient {
   constructor(token, clientInfo) {
+    this.serverName = process.env.SERVER_NAME;
+    this.database = firebase.database();
+    this.webhookURL = `${clientInfo.webhook}?token=${token}`;
+
+    this.clientStatusRef = this.database.ref(
+      `${this.serverName}/tokens/${token}/status`,
+    );
+    this.clientStatusRef.set({
+      token,
+      sessionStatus: 'starting',
+      qrCodeUrl: null,
+      browserStarted: false,
+      isPhoneConnected: false,
+    });
+
     this.token = token;
     this.clientSession = null;
     this.clientInfo = clientInfo;
-    this.serverName = process.env.SERVER_NAME;
-    this.clientData = {
-      token,
-      sessionStatus: 'starting',
-      qrcodeAttempt: 0,
-      qrcode: null,
-      browserStarted: false,
-      isPhoneConnected: false,
-    };
-
-    this.webhookURL = `${this.clientInfo.webhook}?token=${this.token}`;
-
-    this.database = firebase.database();
+    this.browserStarted = false;
 
     this.createClient(token);
   }
@@ -29,14 +32,10 @@ class VenomClient {
 
     Venom.create(
       token,
-      (base64Qr, asciiQR, attempt) => {
-        console.log('\n Update QRCode \n');
-        this.clientData.qrcode = base64Qr;
-        this.clientData.qrcodeAttempt = attempt;
-      },
+      null,
       status => {
         console.log('\n Status Log', status, '\n');
-        this.clientData.sessionStatus = status;
+        this.clientStatusRef.update({ sessionStatus: status });
       },
       {
         logQR: false,
@@ -51,32 +50,29 @@ class VenomClient {
       .then(client => this.start(client))
       .catch(err => {
         console.log('Error on connect', err);
-        this.clientData = {
-          ...this.clientData,
+        this.clientStatusRef.update({
           sessionStatus: 'disconnected',
-          qrcode: null,
-        };
+          qrCodeUrl: null,
+        });
       });
   }
 
   async start(client) {
     console.log('===>', this.token, 'Started');
+    this.browserStarted = true;
     this.clientSession = client;
-    this.clientData.browserStarted = true;
-    this.clientData.isPhoneConnected = true;
-    const sessionInfo = await client.getSessionTokenBrowser();
-    this.clientData.connectionState = await client.getConnectionState();
 
-    this.database
-      .ref(`${this.serverName}/tokens/${this.token}/sessionInfo`)
-      .set(sessionInfo);
+    this.clientStatusRef.update({
+      browserStarted: true,
+      isPhoneConnected: true,
+      connectionState: await client.getConnectionState(),
+    });
 
     client.onMessage(data => {
       this.sendMessageToWebHook(data);
     });
 
     client.onAck(ackData => {
-      console.log('ACK UPDATE');
       if (ackData.ack === 3) {
         axios
           .post(this.webhookURL, {
@@ -92,28 +88,29 @@ class VenomClient {
 
     client.onStateChange(async state => {
       console.log('State Udpate', state);
-      this.clientData.connectionState = state;
+      this.clientStatusRef.update({ connectionState: state });
 
       if (state === 'CONFLICT') {
         client.useHere();
       } else if (state === 'CONNECTED') {
+        this.clientStatusRef.update({ isPhoneConnected: true });
         const updateSessionInfo = await client.getSessionTokenBrowser();
         this.database
           .ref(`${this.serverName}/tokens/${this.token}/sessionInfo`)
           .set(updateSessionInfo);
       } else if (state === 'UNPAIRED') {
-        this.clientData.isPhoneConnected = false;
+        this.clientStatusRef.update({ isPhoneConnected: false });
         this.database
           .ref(`${this.serverName}/tokens/${this.token}/sessionInfo`)
           .remove();
       } else {
-        this.clientData.isPhoneConnected = false;
+        this.clientStatusRef.update({ isPhoneConnected: false });
       }
     });
   }
 
   async sendMessageToClient(data) {
-    if (!this.clientData.browserStarted) throw new Error('Client not started');
+    if (!this.browserStarted) throw new Error('Client not started');
 
     const { number, type, message, url, filename, caption } = data;
 
