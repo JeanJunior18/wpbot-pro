1c4438bd47782ff0b9534b409f99f1baefd566f0
Jean Junior <jclgjunior@gmail.com>
2021-06-28 10:32:20 -0300

    Update node version

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-sudo apt install nodejs
+sudo apt update
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+cat /etc/apt/sources.list.d/nodesource.list
+deb https://deb.nodesource.com/node_14.x focal main
+deb-src https://deb.nodesource.com/node_14.x focal main
+
+sudo apt install -y nodejs
+sudo apt install -y npm
 
 sudo npm install
 
